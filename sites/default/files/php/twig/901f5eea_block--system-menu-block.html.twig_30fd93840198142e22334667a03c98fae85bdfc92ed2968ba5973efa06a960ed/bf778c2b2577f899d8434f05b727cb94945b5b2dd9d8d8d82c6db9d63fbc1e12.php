ea54a0399e2ee6f87096474c27fb26523f56f7d5
Mohammed <despee75020@gmail.com>
2018-01-09 19:05:52 +0100

    rabbi satar

---
+++ sites/default/files/php/twig/901f5eea_block--system-menu-block.html.twig_30fd93840198142e22334667a03c98fae85bdfc92ed2968ba5973efa06a960ed/bf778c2b2577f899d8434f05b727cb94945b5b2dd9d8d8d82c6db9d63fbc1e12.php
@@ -0,0 +1,181 @@
+<?php
+
+/* @classy/block/block--system-menu-block.html.twig */
+class __TwigTemplate_6432f04bde912b2dcf12398da7ba4145a750375a78d4cca5f38151f5eb2c7e4e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_dd7305f3d6022b1a29584cf34a1e1a8209b253ce2d9d17f8aa0e8b3f1f0fe475 = $this->env->getExtension("native_profiler");
+        $__internal_dd7305f3d6022b1a29584cf34a1e1a8209b253ce2d9d17f8aa0e8b3f1f0fe475->enter($__internal_dd7305f3d6022b1a29584cf34a1e1a8209b253ce2d9d17f8aa0e8b3f1f0fe475_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@classy/block/block--system-menu-block.html.twig"));
+
+        $tags = array("set" => 35, "if" => 45, "block" => 53);
+        $filters = array("clean_class" => 39, "clean_id" => 42, "without" => 43);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('set', 'if', 'block'),
+                array('clean_class', 'clean_id', 'without'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 35
+        $context["classes"] = array(0 => "block", 1 => "block-menu", 2 => "navigation", 3 => ("menu--" . \Drupal\Component\Utility\Html::getClass(        // line 39
+(isset($context["derivative_plugin_id"]) ? $context["derivative_plugin_id"] : null))));
+        // line 42
+        $context["heading_id"] = ($this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "id", array()) . \Drupal\Component\Utility\Html::getId("-menu"));
+        // line 43
+        echo "<nav role=\"navigation\" aria-labelledby=\"";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["heading_id"]) ? $context["heading_id"] : null), "html", null, true));
+        echo "\"";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, twig_without($this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "role", "aria-labelledby"), "html", null, true));
+        echo ">
+  ";
+        // line 45
+        echo "  ";
+        if ( !$this->getAttribute((isset($context["configuration"]) ? $context["configuration"] : null), "label_display", array())) {
+            // line 46
+            echo "    ";
+            $context["title_attributes"] = $this->getAttribute((isset($context["title_attributes"]) ? $context["title_attributes"] : null), "addClass", array(0 => "visually-hidden"), "method");
+            // line 47
+            echo "  ";
+        }
+        // line 48
+        echo "  ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_prefix"]) ? $context["title_prefix"] : null), "html", null, true));
+        echo "
+  <h2";
+        // line 49
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["title_attributes"]) ? $context["title_attributes"] : null), "setAttribute", array(0 => "id", 1 => (isset($context["heading_id"]) ? $context["heading_id"] : null)), "method"), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["configuration"]) ? $context["configuration"] : null), "label", array()), "html", null, true));
+        echo "</h2>
+  ";
+        // line 50
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["title_suffix"]) ? $context["title_suffix"] : null), "html", null, true));
+        echo "
+
+  ";
+        // line 53
+        echo "  ";
+        $this->displayBlock('content', $context, $blocks);
+        // line 56
+        echo "</nav>
+";
+        
+        $__internal_dd7305f3d6022b1a29584cf34a1e1a8209b253ce2d9d17f8aa0e8b3f1f0fe475->leave($__internal_dd7305f3d6022b1a29584cf34a1e1a8209b253ce2d9d17f8aa0e8b3f1f0fe475_prof);
+
+    }
+
+    // line 53
+    public function block_content($context, array $blocks = array())
+    {
+        $__internal_bb380d8d3340982a5e111e48699972f808c09008bf4d8e13b3c1624546e9a1fa = $this->env->getExtension("native_profiler");
+        $__internal_bb380d8d3340982a5e111e48699972f808c09008bf4d8e13b3c1624546e9a1fa->enter($__internal_bb380d8d3340982a5e111e48699972f808c09008bf4d8e13b3c1624546e9a1fa_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "content"));
+
+        // line 54
+        echo "    ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+  ";
+        
+        $__internal_bb380d8d3340982a5e111e48699972f808c09008bf4d8e13b3c1624546e9a1fa->leave($__internal_bb380d8d3340982a5e111e48699972f808c09008bf4d8e13b3c1624546e9a1fa_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@classy/block/block--system-menu-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  101 => 54,  95 => 53,  87 => 56,  84 => 53,  79 => 50,  73 => 49,  68 => 48,  65 => 47,  62 => 46,  59 => 45,  52 => 43,  50 => 42,  48 => 39,  47 => 35,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a menu block.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - plugin_id: The ID of the block implementation.*/
+/*  * - label: The configured label of the block if visible.*/
+/*  * - configuration: A list of the block's configuration values.*/
+/*  *   - label: The configured label for the block.*/
+/*  *   - label_display: The display settings for the label.*/
+/*  *   - provider: The module or other provider that provided this block plugin.*/
+/*  *   - Block plugin specific settings will also be stored here.*/
+/*  * - content: The content of this block.*/
+/*  * - attributes: HTML attributes for the containing element.*/
+/*  *   - id: A valid HTML ID and guaranteed unique.*/
+/*  * - title_attributes: HTML attributes for the title element.*/
+/*  * - content_attributes: HTML attributes for the content element.*/
+/*  * - title_prefix: Additional output populated by modules, intended to be*/
+/*  *   displayed in front of the main title tag that appears in the template.*/
+/*  * - title_suffix: Additional output populated by modules, intended to be*/
+/*  *   displayed after the main title tag that appears in the template.*/
+/*  **/
+/*  * Headings should be used on navigation menus that consistently appear on*/
+/*  * multiple pages. When this menu block's label is configured to not be*/
+/*  * displayed, it is automatically made invisible using the 'visually-hidden' CSS*/
+/*  * class, which still keeps it visible for screen-readers and assistive*/
+/*  * technology. Headings allow screen-reader and keyboard only users to navigate*/
+/*  * to or skip the links.*/
+/*  * See http://juicystudio.com/article/screen-readers-display-none.php and*/
+/*  * http://www.w3.org/TR/WCAG-TECHS/H42.html for more information.*/
+/*  *//* */
+/* #}*/
+/* {%*/
+/*   set classes = [*/
+/*     'block',*/
+/*     'block-menu',*/
+/*     'navigation',*/
+/*     'menu--' ~ derivative_plugin_id|clean_class,*/
+/*   ]*/
+/* %}*/
+/* {% set heading_id = attributes.id ~ '-menu'|clean_id %}*/
+/* <nav role="navigation" aria-labelledby="{{ heading_id }}"{{ attributes.addClass(classes)|without('role', 'aria-labelledby') }}>*/
+/*   {# Label. If not displayed, we still provide it for screen readers. #}*/
+/*   {% if not configuration.label_display %}*/
+/*     {% set title_attributes = title_attributes.addClass('visually-hidden') %}*/
+/*   {% endif %}*/
+/*   {{ title_prefix }}*/
+/*   <h2{{ title_attributes.setAttribute('id', heading_id) }}>{{ configuration.label }}</h2>*/
+/*   {{ title_suffix }}*/
+/* */
+/*   {# Menu. #}*/
+/*   {% block content %}*/
+/*     {{ content }}*/
+/*   {% endblock %}*/
+/* </nav>*/
+/* */
