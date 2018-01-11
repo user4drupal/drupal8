813e6a21e5805ec1ef2f1200ee341ce3cecdf82a
Mohammed <despee75020@gmail.com>
2018-01-11 10:24:00 +0100

    rabbi satar

---
+++ sites/default/files/php/twig/901f5eea_form.html.twig_d6e223f260e4b25ab8f460cd06ded257729dbeb2ff7550cee646a76035723abf/a902b294078bbfb55b9ebb116f3d6694f10ed6217402c6708b3695080a7043b2.php
@@ -0,0 +1,90 @@
+<?php
+
+/* core/themes/classy/templates/form/form.html.twig */
+class __TwigTemplate_4ce933b0bc323e77efea6a37f141d4eeeb1617b8a9570e41140ed6d69b577942 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_58eb74dc6f74226157f8d7738ab30f625ce740d5dc6c247475fab21b1c903f73 = $this->env->getExtension("native_profiler");
+        $__internal_58eb74dc6f74226157f8d7738ab30f625ce740d5dc6c247475fab21b1c903f73->enter($__internal_58eb74dc6f74226157f8d7738ab30f625ce740d5dc6c247475fab21b1c903f73_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "core/themes/classy/templates/form/form.html.twig"));
+
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<form";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "
+</form>
+";
+        
+        $__internal_58eb74dc6f74226157f8d7738ab30f625ce740d5dc6c247475fab21b1c903f73->leave($__internal_58eb74dc6f74226157f8d7738ab30f625ce740d5dc6c247475fab21b1c903f73_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  51 => 14,  46 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for a 'form' element.*/
+/*  **/
+/*  * Available variables*/
+/*  * - attributes: A list of HTML attributes for the wrapper element.*/
+/*  * - children: The child elements of the form.*/
+/*  **/
+/*  * @see template_preprocess_form()*/
+/*  *//* */
+/* #}*/
+/* <form{{ attributes }}>*/
+/*   {{ children }}*/
+/* </form>*/
+/* */
